3f4bfac3589fcf889964ac0f869f7e8b040a48b2
manojdeveloper2007 <manoj.developer2007@gmail.com>
2026-07-01 20:45:21 +0530

    strings

---
+++ 13_JavaScript/05_Strings/script.js
@@ -0,0 +1,15 @@
+// String is a sequence of characters ,which is immutable
+
+let my_name = "Manoj";
+
+// Template literal
+console.log(`Hi ! Welcome to galaxy ${my_name}`);
+
+console.log(my_name.toUpperCase());
+console.log(my_name.substring(0,4));
+console.log(my_name.indexOf("a"));
+console.log(my_name.charAt(4));
+console.log(my_name.replace("noj","thesh"));
+
+// using indexing
+console.log(my_name[1]);
